eb37a4f5406e3c8546596af5183a01914bc666f0
Mourat Boutry <mourat@boutry.me>
2021-04-16 13:54:15 +0200

    lesson 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,22 @@ import './App.css';
 import {Todolist} from "./Todolist";
 
 function App() {
+
+    const tasks1 = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 1, title: "RectJS", isDone: false }
+    ]
+    const tasks2 = [
+        { id: 1, title: "Hello world", isDone: true },
+        { id: 2, title: "I'm happy", isDone: false },
+        { id: 1, title: "Yo", isDone: false }
+    ]
+
     return (
         <div className="App">
-            <Todolist/>
-            <Todolist/>
-            <Todolist/>
+            <Todolist title={"What to learn"} tasks={tasks1}/>
+            <Todolist title={"Songs"} tasks={tasks2}/>
         </div>
     );
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 
-export const Todolist = () => {
+type propsType = {
+    title: string,
+    tasks: Array<taskType>
+}
+type taskType = {
+    id: number,
+    isDone: boolean,
+    title: string
+}
+
+export const Todolist = ({title, tasks}:propsType) => {
     return (
         <div>
-            <h3>What to learn</h3>
+            <h3>{title}</h3>
             <div>
                 <input/>
                 <button>+</button>
             </div>
             <ul>
-                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                <li><input type="checkbox" checked={tasks[0].isDone}/> <span>{tasks[0].title}</span></li>
+                <li><input type="checkbox" checked={tasks[1].isDone}/> <span>{tasks[1].title}</span></li>
+                <li><input type="checkbox" checked={tasks[2].isDone}/> <span>{tasks[2].title}</span></li>
             </ul>
             <div>
                 <button>All</button>
